670223be8af84a105688c6b08bb6aee5c148f347
verhowyna <verhowyna@ukr.net>
2023-09-17 23:41:57 +0300

    sunday-6

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+function saveCurrentTime(time) {
+    localStorage.setItem('videoplayer-current-time', time);
+}
+
+document.getElementById('startVideoButton').addEventListener('click', () => {
+    player.play().catch((error) => {
+        console.log('Помилка відтворення відео', error);
+    });
+});
+
+player.on('timeupdate', _.throttle(function (evt) {
+    const currentTime = Math.floor(evt.seconds);
+    saveCurrentTime(currentTime);
+}, 1000));
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+    player.setCurrentTime(savedTime);
+}
