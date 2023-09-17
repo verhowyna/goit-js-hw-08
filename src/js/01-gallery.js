8634d04fc3336647392a583d33afff4d5ef6cc7f
verhowyna <verhowyna@ukr.net>
2023-09-17 12:46:43 +0300

    sunday-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,15 +1,18 @@
-import SimpleLightbox from 'simplelightbox'
+import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 const galleryList = document.querySelector('.gallery');
+
 const markup = galleryItems.map(({ preview, original, description }) => `<li class="gallery__item">
    <a class="gallery__link" href="${original}">
       <img class="gallery__image" src="${preview}" alt="${description}" />
    </a>
 </li>`).join('');
+
 galleryList.insertAdjacentHTML("beforeend", markup);
 
 const lightbox = new SimpleLightbox(".gallery a", {
