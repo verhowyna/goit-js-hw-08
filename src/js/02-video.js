9779ef74754a50ff9900095974f5890fe0322cd5
verhowyna <verhowyna@ukr.net>
2023-09-21 08:29:44 +0300

    commit-1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,18 +8,3 @@ function saveCurrentTime(time) {
     localStorage.setItem('videoplayer-current-time', time);
 }
 
-document.getElementById('startVideoButton').addEventListener('click', () => {
-    player.play().catch((error) => {
-        console.log('Помилка відтворення відео', error);
-    });
-});
-
-player.on('timeupdate', _.throttle(function (evt) {
-    const currentTime = Math.floor(evt.seconds);
-    saveCurrentTime(currentTime);
-}, 1000));
-
-const savedTime = localStorage.getItem('videoplayer-current-time');
-if (savedTime) {
-    player.setCurrentTime(savedTime);
-}
